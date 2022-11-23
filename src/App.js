716e1b2b030652cbfd0b8dcad3d78d25dee2c1b4
farhadbgd <farhadbgd@outlook.com>
2022-11-24 00:51:38 +0600

    toast install

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
+  const tostHandler = () => {
+    toast.success('hello world')
+  }
   return (
     <div className="App">
-      <button className="btn">Button</button>
+      <button onClick={tostHandler} className="btn">Button</button>
       <button className="btn btn-primary">Button</button>
       <button className="btn btn-secondary">Button</button>
       <button className="btn btn-accent">Button</button>
